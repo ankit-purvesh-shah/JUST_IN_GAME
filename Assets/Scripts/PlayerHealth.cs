2c3cfb2be091e4ee86cce17feb6481b8293cd7c9
ankit_purvesh_shah <ankitpur@usc.edu>
2022-03-04 12:08:52 -0800

    New Feature to add health healing on Reward Collection. Fixed Health heal of 150 points.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -16,11 +18,20 @@ public class PlayerHealth : MonoBehaviour
     private float enemyMass = 0;
     PlayerHealth playerHealth;
     GameObject playerObject;
+
+
+    // Rewards Health Heal
+    private bool rewardsCollected = false;
+    private readonly float healFactor=150.0F;
+    public TextMeshProUGUI countText;
+    private int count;
     // Start is called before the first frame update
     void Start()
     {
         currenthealth = maxhealth;
         healthBar.SetMaxHealth(maxhealth);
+        count = 0;
+        SetCountText();
     }
     private void Awake()
     {
@@ -34,8 +45,17 @@ public class PlayerHealth : MonoBehaviour
         healthBar.SetHealth(currenthealth);
     }
 
+
+    void HealHealth(float healXP)
+    {
+        Debug.Log(currenthealth);
+        currenthealth += healXP;
+        Debug.Log(currenthealth);
+        healthBar.SetHealth(currenthealth);
+    }
+
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
         if (collided && enemyMass != 0)
@@ -55,12 +75,24 @@ public class PlayerHealth : MonoBehaviour
             enemyMass = collision.rigidbody.mass;
         }
     }
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+    }
 
     private void OnCollisionExit(Collision collision)
     {
+        Debug.Log(collision.gameObject.tag);
         if (collision.gameObject.tag == "Enemy")
         {
             collided = false;
+        }
+        if (collision.gameObject.CompareTag("Reward"))
+        {
+            collision.gameObject.SetActive(false);
+            HealHealth(healFactor);
+            count = count + 1;
+            SetCountText();
         }
     }
 }
